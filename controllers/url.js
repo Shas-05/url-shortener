9dd9102bff3c8497771fa8bde12c2dca0fc4c97d
Shas-05 <shashank.cms.2005@gmail.com>
2026-03-24 00:25:29 +0530

    New commits

---
+++ controllers/url.js
@@ -0,0 +1,16 @@
+const {} = require("nanoid")
+const URL = require('../models/url');
+async function handleGenerateNewShortUrl(req, res) {
+    const body = req.body;
+    if(!body.url) return res.status(400).json({error: "URL is required"});
+    const shortID = nanoid(8);
+    await URL.create({
+        shortID: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+    });
+
+    return res.json({id: shortID});
+}
+
+module.exports
