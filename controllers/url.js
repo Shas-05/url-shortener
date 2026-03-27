8355f2bab92343086171953ddd67578560276de7
Shas-05 <shashank.cms.2005@gmail.com>
2026-03-27 19:52:04 +0530

    Final Commit

--- controllers/url.js
+++ controllers/url.js
@@ -14,6 +14,13 @@ async function handleGenerateNewShortUrl(req, res) {
     return res.json({id: shortID});
 }
 
+async function handleGetAnalytics(req, res) {
+    const shrotId = req.parmas.shortId;
+    const result = await URL.findOne({shortId});
+    return res.json({totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory})
+}
 module.exports = {
   handleGenerateNewShortUrl,
+  handleGetAnalytics,
 };
